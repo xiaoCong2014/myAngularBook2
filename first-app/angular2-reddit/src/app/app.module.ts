d3e7402590cc0b4cbf099c779a30a39c96349bb6
1149023208@qq.com <zxxxxx11>
2017-12-02 22:05:26 +0800

    初始化 angular2-reddit

--- first-app/angular2-reddit/src/app/app.module.ts
+++ first-app/angular2-reddit/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-
+import { ArticleComponent } from './article/article.component.ts';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent // <-- added this
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
